c98b3dbfc3363516ae4180431f8c9e0f64b228bc
adee <362889298@qq.com>
2016-08-24 22:53:43 +0800

    创建了car类

---
+++ weibo/weibo/Car.h
@@ -0,0 +1,13 @@
+//
+//  Car.h
+//  weibo
+//
+//  Created by addychan on 16/8/24.
+//  Copyright © 2016年 addychan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Car : NSObject
+
+@end
---
+++ weibo/weibo/Car.m
@@ -0,0 +1,13 @@
+//
+//  Car.m
+//  weibo
+//
+//  Created by addychan on 16/8/24.
+//  Copyright © 2016年 addychan. All rights reserved.
+//
+
+#import "Car.h"
+
+@implementation Car
+
+@end
