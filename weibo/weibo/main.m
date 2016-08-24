8101464c7524b0d0591ef08767be4646e20e5557
adee <362889298@qq.com>
2016-08-24 22:48:21 +0800

    初始化项目

---
+++ weibo/weibo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  weibo
+//
+//  Created by addychan on 16/8/24.
+//  Copyright © 2016年 addychan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
